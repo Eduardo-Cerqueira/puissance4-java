d634e59f5e5c81b9b026dd242a3732d85a048daa
Val <Val@email.com>
2023-01-16 15:09:20 +0100

    first commit

---
+++ src/model/Ia.java
@@ -0,0 +1,21 @@
+package model;
+
+import java.lang.reflect.Constructor;
+
+public class Ia {
+
+    private String name;
+    private String symbol;
+    private String difficulty;
+    private String[][] matrice;
+
+    public Ia(String name, String symbol, String difficulty, String[][] matrice) {
+
+        name = name;
+        symbol = symbol;
+        difficulty = difficulty;
+        matrice = matrice;
+
+    }
+
+}
